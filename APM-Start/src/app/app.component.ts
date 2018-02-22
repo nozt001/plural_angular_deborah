e7b47799e41ad5601137b08ae7123e7558e3926c
Niyazi Ozturk <nozt001@aucklanduni.ac.nz>
2018-02-22 20:26:28 +1300

    Product list component added to the project, CH-4

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from "@angular/core";
 
+
 @Component({
   selector: 'pm-root',
   template: `
     <div>
       <h1>{{pageTitle}}</h1>
-      <div>My First Component</div>
-      <p>Build Android apps using the popular and efficient Android Studio 3 suite of tools,
-       an integrated development environment (IDE) for Android developers using Java APIs. 
-       With this book, you’ll learn the latest and most productive tools in the Android tools ecosystem, 
-       ensuring quick Android app development and minimal…</p>
+      <pm-products></pm-products>
     </div>
-    
+
   `
 })
 export class AppComponent {
